f3bfe654f15dd7d993ce725e860256b97e68dedb
Jetlove19 <rbrorlove@gmail.com>
2023-02-23 20:53:51 +0300

    Добавьте файлы проекта.

---
+++ 2finalTask8module/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace _2finalTask8module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"\Users";
+            Console.WriteLine($"Размер папки {path} - {ShowFolderSize(path)} байт");
+        }
+
+        static long ShowFolderSize(string path)
+        {
+            long folderSize = 0;
+            try
+            {
+                DirectoryInfo dirinfo = new DirectoryInfo(path);
+                if (dirinfo.Exists)
+                {
+                    FileInfo[] files = dirinfo.GetFiles();
+                    foreach (var item in files)
+                    {
+                        folderSize += item.Length;
+                    }
+                    DirectoryInfo[] subfolders = dirinfo.GetDirectories();
+                    foreach (var item in subfolders)
+                    {
+                        folderSize += ShowFolderSize(item.FullName);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Указан неверный путь к директории");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return folderSize;
+        }
+    }
+}
